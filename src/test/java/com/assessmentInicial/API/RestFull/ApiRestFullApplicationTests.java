15636fabbe20038045c304c393ab4d3065006c95
Luis Fuentes <luis.utl.15@gmail.com>
2024-03-28 12:55:04 -0600

    Hola mundo

---
+++ src/test/java/com/assessmentInicial/API/RestFull/ApiRestFullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessmentInicial.API.RestFull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestFullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
